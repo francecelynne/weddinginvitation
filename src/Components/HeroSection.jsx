03540ad798973626d950004c7e6ca439d85c8b13
francecelynne <celynnefrance2025@gmail.com>
2026-03-22 20:46:57 +0800

    SomeThingNew

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 
 // import bgMusic from '../src/assets/audio/goodness-of-god-piano-instrumenta.org.mp3';
-import bgMusic from '../public/audio/goodness-of-god-piano.mp3';
+import bgMusic from '.audio/goodness-of-god-piano.mp3';
 function HeroSection() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <section>
-        <div className="h-screen bg-[url('../src/assets/img/kiss.jpg')] bg-no-repeat bg-center 
+        <div className="h-screen bg-[url('./img/kiss.jpg')] bg-no-repeat bg-center 
                         bg-cover grayscale flex justify-center items-start">
             <div className='flex flex-col items-center gap-5 w-screen bg-linear-to-b from-black pt-20! h-screen'>
                 <span className='text-white'>A WEDDING OF</span>
