7b216aa455fb3e50cc62960feb3cb2baad92547b
francecelynne <celynnefrance2025@gmail.com>
2026-03-22 17:54:37 +0800

    Update Image at Location

--- src/Components/Location.jsx
+++ src/Components/Location.jsx
@@ -14,8 +14,8 @@ function Location() {
                 <strong>Heritage 179</strong><br/>Balubaran St, San Rafael, Bulacan
             </span>
             <div className='flex gap-5 items-center flex-col md:flex-row'>
-                <div className="md:w-6/12 w-12/12 h-70  md:h-125">
-                    <img src="../src/assets/img/TheHeritage179.jpg" height='' width='cover' className='h-full w-cover'/>
+                <div className="md:w-6/12 w-12/12 h-70  md:h-125 bg-[url('../src/assets/img/TheHeritage179.jpg')]">
+                    {/* <img src="../src/assets/img/TheHeritage179.jpg" height='' width='cover' className='h-full w-cover'/> */}
                     {/* <video
                         className="w-full h-full object-cover"
                         src="../src/assets/video/TheHeritage179.mp4"
